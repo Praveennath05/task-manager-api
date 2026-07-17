ffcdf6f72902cbe27e517741bab8f14d58470304
Praveennath <praveennath052004@gmail.com>
2026-07-17 13:08:22 +0530

    feat: Added AuthService file

---
+++ TaskManager.Web/Services/AuthService.cs
@@ -0,0 +1,20 @@
+using System.Net.Http.Json;
+using TaskManager.Web.Models;
+
+namespace TaskManager.Web.Services;
+public class AuthService
+{
+    private readonly HttpClient _httpClient;
+    public AuthService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+    public async Task<AuthResponse?>LoginAsync(string email, string password)
+    {
+        var request = new LoginRequest{Email = email, Password = password};
+        var response = await _httpClient.PatchAsJsonAsync("/api/Auth/login", request);
+        if(!response.IsSuccessStatusCode)
+        return null;
+        return await response.Content.ReadFromJsonAsync<AuthResponse>();
+    }
+}
