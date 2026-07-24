206c4f30e6d3602debc735e7955ddff1cddcca21
Praveennath <praveennath052004@gmail.com>
2026-07-24 22:43:03 +0530

    Updated register path

--- TaskManager.Client/src/App.jsx
+++ TaskManager.Client/src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import Login from './pages/Login.jsx';
 import Tasks from './pages/Tasks.jsx';
+import Register from './pages/Register.jsx';
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/" element= {<Tasks/>}/>
+      <Route path="/register" element={<Register />} />
     </Routes>
   );
 }
